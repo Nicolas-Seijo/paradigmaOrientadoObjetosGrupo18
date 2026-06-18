a3097270fbc9948215b6879bbe0db805c42e65dd
niseijo-dev <niicoseijo@gmail.com>
2026-06-17 21:18:04 -0300

    Cambios en la lógica y en la estética

--- src/views/MovimientoView.java
+++ src/views/MovimientoView.java
@@ -1,8 +1,6 @@
 package views;
 
 import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -11,9 +9,11 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+import javax.swing.border.Border;
 
 import controlador.Controlador;
 import modelo.BarcoEnemigo;
@@ -28,12 +28,8 @@ public class MovimientoView extends JPanel implements KeyListener, ActionListene
 
     private List<ExplosionVisual> listaExplosionesVisuales;
 
-    private static final int Y_SUPERFICIE_AGUA = 100;
     private static final Color COLOR_AGUA = new Color(28, 107, 160);
-    private static final Color COLOR_SUPERFICIE_CIELO = new Color(135, 206, 235);
-    private static final Color COLOR_BARCO = new Color(64, 64, 64);
-    private static final Color COLOR_CARGA = new Color(178, 34, 34);
-    private static final Color COLOR_SUBMARINO = Color.ORANGE;
+    private static final Color COLOR_CARGA_MILITAR = new Color(85, 107, 47);
 
     public MovimientoView(Controlador controlador) {
         this.controlador = controlador;
@@ -46,7 +42,6 @@ public class MovimientoView extends JPanel implements KeyListener, ActionListene
         etiquetaDatosEstado = new JLabel("");
         etiquetaDatosEstado.setForeground(Color.WHITE);
         etiquetaDatosEstado.setBounds(10, 10, 800, 30);
-        add(etiquetaDatosEstado);
 
         timer = new Timer(50, this);
         timer.start();
@@ -67,7 +62,11 @@ public class MovimientoView extends JPanel implements KeyListener, ActionListene
         } else {
             timer.stop();
             etiquetaDatosEstado.setText("JUEGO TERMINADO - Puntaje Final: " + controlador.getJuego().getPuntaje());
-            repaint();
+
+            this.removeAll();
+            this.add(etiquetaDatosEstado);
+            this.revalidate();
+            this.repaint();
         }
     }
 
@@ -83,84 +82,161 @@ public class MovimientoView extends JPanel implements KeyListener, ActionListene
     }
 
     private void dibujarEstado() {
+        this.removeAll();
+
         String textoEstado = String.format("Nivel: %d | Puntaje: %d | Vidas: %d | Salud: %d",
                 controlador.getJuego().getNivel(),
                 controlador.getJuego().getPuntaje(),
                 controlador.getJuego().getSubmarino().getVidas(),
                 controlador.getJuego().getSubmarino().getSalud());
         etiquetaDatosEstado.setText(textoEstado);
-
-        repaint();
-    }
-
-    @Override
-    protected void paintComponent(Graphics graficosBase) {
-        super.paintComponent(graficosBase);
-        Graphics2D graficos2D = (Graphics2D) graficosBase;
-
-        graficos2D.setColor(COLOR_SUPERFICIE_CIELO);
-        graficos2D.fillRect(0, 0, getWidth(), Y_SUPERFICIE_AGUA);
-        graficos2D.setColor(Color.WHITE);
-        graficos2D.drawLine(0, Y_SUPERFICIE_AGUA, getWidth(), Y_SUPERFICIE_AGUA);
-
-        graficos2D.setColor(COLOR_BARCO);
-        for (BarcoEnemigo barco : controlador.getJuego().getBarcos()) {
-            int posicionXBarco = barco.getPosicionX();
-            int posicionYBarco = 70;
-            graficos2D.fillRect(posicionXBarco + 15, posicionYBarco - 15, 20, 15);
-            graficos2D.fillRect(posicionXBarco, posicionYBarco, 80, 30);
-        }
-
-        graficos2D.setColor(COLOR_CARGA);
-        for (CargaProfundidad carga : controlador.getJuego().getCargas()) {
-            graficos2D.fillOval(carga.getPosX(), carga.getPosY(), 20, 20);
-        }
+        this.add(etiquetaDatosEstado);
 
         Submarino submarino = controlador.getJuego().getSubmarino();
         if (submarino != null) {
-            int posicionXSubmarino = submarino.getPosX();
-            int posicionYSubmarino = submarino.getPosY();
+            JPanel panelSubmarino = new JPanel();
+            panelSubmarino.setLayout(null);
+            panelSubmarino.setOpaque(false);
 
-            graficos2D.setColor(COLOR_SUBMARINO);
-            graficos2D.fillRect(posicionXSubmarino + 20, posicionYSubmarino - 15, 20, 15);
+            panelSubmarino.setBounds(submarino.getPosX(), submarino.getPosY() - 15, 90, 45);
 
-            graficos2D.fillRoundRect(posicionXSubmarino, posicionYSubmarino, 60, 30, 15, 15);
+            Border bordeRojo = BorderFactory.createLineBorder(Color.RED);
 
-            graficos2D.setColor(new Color(135, 206, 235));
-            graficos2D.fillRoundRect(posicionXSubmarino + 15, posicionYSubmarino + 10, 10, 10, 4, 4);
-            graficos2D.fillRoundRect(posicionXSubmarino + 35, posicionYSubmarino + 10, 10, 10, 4, 4);
+            // Punta izquierda modificada: menos ancha y centrada respecto al cuerpo
+            JLabel punta = new JLabel();
+            punta.setOpaque(true);
+            punta.setBackground(Color.ORANGE);
+            punta.setBorder(bordeRojo);
+            punta.setBounds(4, 21, 11, 21);
+            panelSubmarino.add(punta);
 
-            graficos2D.setColor(Color.RED);
-            graficos2D.drawRect(posicionXSubmarino + 20, posicionYSubmarino - 15, 20, 15);
-            graficos2D.drawRoundRect(posicionXSubmarino, posicionYSubmarino, 60, 30, 15, 15);
+            JLabel ventana1 = new JLabel();
+            ventana1.setOpaque(true);
+            ventana1.setBackground(new Color(135, 206, 235));
+            ventana1.setBounds(30, 25, 10, 10);
+            panelSubmarino.add(ventana1);
+
+            JLabel ventana2 = new JLabel();
+            ventana2.setOpaque(true);
+            ventana2.setBackground(new Color(135, 206, 235));
+            ventana2.setBounds(50, 25, 10, 10);
+            panelSubmarino.add(ventana2);
+
+            JLabel heliceArriba = new JLabel();
+            heliceArriba.setOpaque(true);
+            heliceArriba.setBackground(Color.ORANGE);
+            heliceArriba.setBorder(bordeRojo);
+            heliceArriba.setBounds(75, 18, 10, 10);
+            panelSubmarino.add(heliceArriba);
+
+            JLabel heliceAbajo = new JLabel();
+            heliceAbajo.setOpaque(true);
+            heliceAbajo.setBackground(Color.ORANGE);
+            heliceAbajo.setBorder(bordeRojo);
+            heliceAbajo.setBounds(75, 32, 10, 10);
+            panelSubmarino.add(heliceAbajo);
+
+            JLabel torreta = new JLabel();
+            torreta.setOpaque(true);
+            torreta.setBackground(Color.ORANGE);
+            torreta.setBorder(bordeRojo);
+            torreta.setBounds(35, 0, 20, 15);
+            panelSubmarino.add(torreta);
+
+            JLabel cuerpo = new JLabel();
+            cuerpo.setOpaque(true);
+            cuerpo.setBackground(Color.ORANGE);
+            cuerpo.setBorder(bordeRojo);
+            cuerpo.setBounds(15, 15, 60, 30);
+            panelSubmarino.add(cuerpo);
+
+            this.add(panelSubmarino);
         }
-    }
 
-    @Override
-    public void paint(Graphics graficosBase) {
-        super.paint(graficosBase);
-        Graphics2D graficos2D = (Graphics2D) graficosBase;
+        for (BarcoEnemigo barco : controlador.getJuego().getBarcos()) {
+            JPanel panelBarco = new JPanel();
+            panelBarco.setLayout(null);
+            panelBarco.setOpaque(false);
+            panelBarco.setBounds(barco.getPosicionX() - 10, 55, 100, 45);
+
+            JLabel ventanaBarco = new JLabel();
+            ventanaBarco.setOpaque(true);
+            ventanaBarco.setBackground(Color.WHITE);
+            ventanaBarco.setBounds(25, 20, 20, 10);
+            panelBarco.add(ventanaBarco);
+
+            JLabel cabina = new JLabel();
+            cabina.setOpaque(true);
+            cabina.setBackground(Color.DARK_GRAY);
+            cabina.setBounds(20, 0, 30, 15);
+            panelBarco.add(cabina);
+
+            JLabel extensionIzquierda = new JLabel();
+            extensionIzquierda.setOpaque(true);
+            extensionIzquierda.setBackground(Color.DARK_GRAY);
+            extensionIzquierda.setBounds(0, 15, 10, 20);
+            panelBarco.add(extensionIzquierda);
+
+            JLabel cuerpoBarco = new JLabel();
+            cuerpoBarco.setOpaque(true);
+            cuerpoBarco.setBackground(Color.DARK_GRAY);
+            cuerpoBarco.setBounds(10, 15, 80, 30);
+            panelBarco.add(cuerpoBarco);
+
+            JLabel extensionDerecha = new JLabel();
+            extensionDerecha.setOpaque(true);
+            extensionDerecha.setBackground(Color.DARK_GRAY);
+            extensionDerecha.setBounds(90, 15, 10, 20);
+            panelBarco.add(extensionDerecha);
+
+            this.add(panelBarco);
+        }
+
+        for (CargaProfundidad carga : controlador.getJuego().getCargas()) {
+            JLabel etiquetaCarga = new JLabel();
+            etiquetaCarga.setOpaque(true);
+            etiquetaCarga.setBackground(COLOR_CARGA_MILITAR);
+            etiquetaCarga.setBounds(carga.getPosX(), carga.getPosY(), 20, 20);
+            this.add(etiquetaCarga);
+        }
+
         for (ExplosionVisual explosion : listaExplosionesVisuales) {
-            explosion.dibujar(graficos2D);
+            int ticksTranscurridos = ExplosionVisual.TICKS_TOTALES - explosion.ticksRestantes;
+            int tamanoActual = 10 + (ticksTranscurridos * 6);
+
+            JLabel etiquetaExplosion = new JLabel();
+            etiquetaExplosion.setOpaque(true);
+            etiquetaExplosion.setBackground(Color.YELLOW);
+            etiquetaExplosion.setBounds(explosion.centroX - tamanoActual / 2, explosion.centroY - tamanoActual / 2, tamanoActual, tamanoActual);
+            this.add(etiquetaExplosion);
         }
+
+        JLabel etiquetaCielo = new JLabel();
+        etiquetaCielo.setOpaque(true);
+        etiquetaCielo.setBackground(new Color(135, 206, 235));
+        etiquetaCielo.setBounds(0, 0, 1000, 100);
+        this.add(etiquetaCielo);
+
+        this.revalidate();
+        this.repaint();
     }
 
     @Override
-    public void keyPressed(KeyEvent eventoTeclado) {
-        int codigoTecla = eventoTeclado.getKeyCode();
+    public void keyPressed(KeyEvent e) {
+        int codigoTecla = e.getKeyCode();
         if (codigoTecla == KeyEvent.VK_UP || codigoTecla == KeyEvent.VK_W) controlador.moverArriba();
         else if (codigoTecla == KeyEvent.VK_DOWN || codigoTecla == KeyEvent.VK_S) controlador.moverAbajo();
         else if (codigoTecla == KeyEvent.VK_LEFT || codigoTecla == KeyEvent.VK_A) controlador.moverIzquierda();
         else if (codigoTecla == KeyEvent.VK_RIGHT || codigoTecla == KeyEvent.VK_D) controlador.moverDerecha();
     }
 
-    @Override public void keyReleased(KeyEvent eventoTeclado) {}
-    @Override public void keyTyped(KeyEvent eventoTeclado) {}
+    @Override public void keyReleased(KeyEvent e) {}
+    @Override public void keyTyped(KeyEvent e) {}
 
     private class ExplosionVisual {
         int centroX, centroY;
         int ticksRestantes;
-        final int TICKS_TOTALES = 8;
+        public static final int TICKS_TOTALES = 8;
 
         public ExplosionVisual(int centroX, int centroY) {
             this.centroX = centroX;
@@ -176,26 +252,6 @@ public class MovimientoView extends JPanel implements KeyListener, ActionListene
 
         public boolean estaTerminada() {
             return ticksRestantes <= 0;
-        }
-
-        public void dibujar(Graphics2D graficos2D) {
-            int ticksTranscurridos = TICKS_TOTALES - ticksRestantes;
-            int tamanoFinal = 60;
-            int tamanoInicial = 10;
-
-            float proporcionTranscurrida = (float) ticksTranscurridos / TICKS_TOTALES;
-            int tamanoActual = tamanoInicial + (int) ((tamanoFinal - tamanoInicial) * proporcionTranscurrida);
-
-            int intensidadRojo = 255;
-            int intensidadVerde = (int)(255 * (1 - proporcionTranscurrida));
-            int intensidadAzul = 0;
-            int nivelTransparencia = (int)(255 * (1 - proporcionTranscurrida * proporcionTranscurrida));
-
-            graficos2D.setColor(new Color(intensidadRojo, Math.max(0, intensidadVerde), intensidadAzul, Math.max(0, nivelTransparencia)));
-            graficos2D.fillOval(centroX - tamanoActual/2, centroY - tamanoActual/2, tamanoActual, tamanoActual);
-
-            graficos2D.setColor(new Color(255, 255, 255, nivelTransparencia/2));
-            graficos2D.drawOval(centroX - tamanoActual/2, centroY - tamanoActual/2, tamanoActual, tamanoActual);
         }
     }
 }
